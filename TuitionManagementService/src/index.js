8ed630b9c81521d3e9a8042c291eb29126cc4e50
luribyLOCAL <vanhoi11061999@gmail.com>
2022-07-05 04:11:49 +0700

    update feature

--- TuitionManagementService/src/index.js
+++ TuitionManagementService/src/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const XLSX = require('xlsx')
-const file = XLSX.readFile('./public/tuition-list.xlsx')
+const file = XLSX.readFile(__dirname + '/public/tuition-list.xlsx')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
@@ -11,8 +11,23 @@ const route = require('./routes/RoutesIndex')
 app.use(morgan('combined'))
 
 app.get("/",(req,res) => {
-  console.log(file);
+  const sheets = file.SheetNames;
+
+  const data = [];
+  for (let i = 0; i < sheets.length; i++)
+  {
+    const sheetname = sheets[i];
+
+    const sheetdata = XLSX.utils.sheet_to_json(file.Sheets[sheetname]);
+
+    sheetdata.forEach((a) => {
+      data.push(a);
+    });
+  }
+  console.log(data);
+  res.send(data);
 })
+
 //routes init
 route(app);
 
