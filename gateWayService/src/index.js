0f2fb855ba07abe583c99d4af702c27bef3db7d6
Hải Nam <hainam@Terminal.local>
2022-07-06 01:05:07 +0700

    update http method

--- gateWayService/src/index.js
+++ gateWayService/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const proxy = require('express-http-proxy');
+const cors = require('cors');
+
 const { 
     API_URL, 
     PORT_GATEWAY, 
     PORT_SERVICE_AUTH, 
     PORT_SERVICE_STUDENT 
 } = require('../../config');
+
+app.use(cors());
 
 const checkHealth = (req, res) => {
     res.send({
@@ -15,6 +20,8 @@ const checkHealth = (req, res) => {
 }
 
 app.get('/', checkHealth);
+app.use('/auth', proxy(`${API_URL}:${PORT_SERVICE_AUTH}`));
+app.use('/student', proxy(`${API_URL}:${PORT_SERVICE_STUDENT}`));
 
 app.listen(PORT_GATEWAY, err => {
     if (err) console.log(err);
