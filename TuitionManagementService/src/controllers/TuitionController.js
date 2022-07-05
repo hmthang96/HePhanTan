218cdd2ec1f67f3891a355710eae402926c85a12
luribyLOCAL <vanhoi11061999@gmail.com>
2022-07-05 11:21:58 +0700

    fix some bug

--- TuitionManagementService/src/controllers/TuitionController.js
+++ TuitionManagementService/src/controllers/TuitionController.js
@@ -1,30 +1,32 @@
+const path = require('path')
+const XLSX = require('xlsx')
+const file = XLSX.readFile(process.cwd() + '/src/public/tuition-list.xlsx')
 
-// const XLSX = require('xlsx')
-// const file = XLSX.readFile(__dirname + '/public/tuition-list.xlsx')
+//console.log(process.cwd());
 class TuitionController{
     //[GET] /tuition
-    // index(req, res)
-    // {
-    //     const sheets = file.SheetNames;
+    index(req, res)
+    {
+        const sheets = file.SheetNames;
 
-    //     const data = [];
-    //     for (let i = 0; i < sheets.length; i++)
-    //     {
-    //       const sheetname = sheets[i];
+        const data = [];
+        for (let i = 0; i < sheets.length; i++)
+        {
+          const sheetname = sheets[i];
       
-    //       const sheetdata = XLSX.utils.sheet_to_json(file.Sheets[sheetname]);
+          const sheetdata = XLSX.utils.sheet_to_json(file.Sheets[sheetname]);
       
-    //       sheetdata.forEach((a) => {
-    //         data.push(a);
-    //       });
-    //     }
-    //     console.log(data);
-    //     res.send(data);
-    // }
-
-    index(req,res){
-        res.send('tuition')
+          sheetdata.forEach((a) => {
+            data.push(a);
+          });
+        }
+        console.log(data);
+        res.send(data);
     }
+
+    // index(req,res){
+    //     res.send('tuition')
+    // }
 
 }
 
--- TuitionManagementService/src/index.js
+++ TuitionManagementService/src/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
-const XLSX = require('xlsx')
-const file = XLSX.readFile(__dirname + '/public/tuition-list.xlsx')
+// const XLSX = require('xlsx')
+// const file = XLSX.readFile(__dirname + '/public/tuition-list.xlsx')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
@@ -10,23 +10,23 @@ const route = require('./routes/RoutesIndex')
 
 app.use(morgan('combined'))
 
-app.get("/",(req,res) => {
-  const sheets = file.SheetNames;
+// app.get("/",(req,res) => {
+//   const sheets = file.SheetNames;
 
-  const data = [];
-  for (let i = 0; i < sheets.length; i++)
-  {
-    const sheetname = sheets[i];
+//   const data = [];
+//   for (let i = 0; i < sheets.length; i++)
+//   {
+//     const sheetname = sheets[i];
 
-    const sheetdata = XLSX.utils.sheet_to_json(file.Sheets[sheetname]);
+//     const sheetdata = XLSX.utils.sheet_to_json(file.Sheets[sheetname]);
 
-    sheetdata.forEach((a) => {
-      data.push(a);
-    });
-  }
-  console.log(data);
-  res.send(data);
-})
+//     sheetdata.forEach((a) => {
+//       data.push(a);
+//     });
+//   }
+//   console.log(data);
+//   res.send(data);
+// })
 
 //routes init
 route(app);
